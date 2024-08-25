59af2806b4c4b91fd6e811cef9d6421917601d1d
Aashif-k <ashif.aachi.k@gmail.com>
2024-08-26 00:55:36 +0530

    app.py

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ import numpy as np
 import pickle
 pickle_in = open('LR.pkl','rb')
 LR = pickle.load(pickle_in)
-def predic(Pclass,Sex,Age,SibSp,Parch,Fare,Embarked):
-    prediction = LR.predict([[Pclass,Sex,Age,SibSp,Parch,Fare,Embarked]])
-    print(prediction)
-    return prediction
 def main():
   st.title("titanic prediction")
   Pclass=st.text_input("pclass")
@@ -32,3 +28,7 @@ def main():
   st.success('The output is {}'.format(result))
 if __name__=='__main__':
   main()
+def predic(Pclass,Sex,Age,SibSp,Parch,Fare,Embarked):
+    prediction = LR.predict([[Pclass,Sex,Age,SibSp,Parch,Fare,Embarked]])
+    print(prediction)
+    return prediction
